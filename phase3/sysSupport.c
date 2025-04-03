e4edd7d96b29712c20ff7a8bf04b0436c69c37f9
lukabagash <bagash_l2@denison.edu>
2025-04-03 17:10:48 -0400

    Finalize sys skeleton

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -47,13 +47,36 @@ HIDDEN void writeTerminal(char *virtAddr, int len) {
        
     for (int i = 0; i < len; i++) {
         // Write printer device's DATA0 field with printer device address (i.e., address of printer device)
-        printerdev.data0 = virtAddr[i];
+        printerdev.d_status = ALLOFF | printerdev.d_status | (virtAddr[i] << 8);
         printerdev.d_command = 2; /* PRINTCHR command code */
-        
+
         /* do we issue sys5? to suspend u_proc */
 
         /* if not successfully written PRINTERRRR status code */
-        if (printerdev.d_status = 4) {
+        if ((printerdev.d_status & 0x0000000F)= 4) {
+            charNum = -printerdev.d_status;
+            break;
+        }
+        charNum++;
+    } 
+    //Resume back to user mode with v0 updated accordingly:
+    currentProcess->p_s.s_v0 = charNum;
+}
+
+HIDDEN void readTerminal(char *virtAddr){
+    int charNum = 0;
+    devregarea_t reg = RAMBASEADDR;
+    device_t printerdev = reg->devreg[TERMINT];
+       
+    for (int i = 0; i < len; i++) {
+        // Write printer device's DATA0 field with printer device address (i.e., address of printer device)
+        printerdev.d_data0 = ALLOFF | printerdev.d_data0 | (virtAddr[i] << 8);
+        printerdev.d_data1 = 2; /* PRINTCHR command code */
+
+        /* do we issue sys5? to suspend u_proc */
+
+        /* if not successfully written PRINTERRRR status code */
+        if ((printerdev.d_data0 & 0x0000000F) == 4) {
             charNum = -printerdev.d_status;
             break;
         }
