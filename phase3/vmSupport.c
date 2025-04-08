aa1acfd35831186f98fae63b5bc607ea030a6998
lukabagash <bagash_l2@denison.edu>
2025-04-08 19:15:56 -0400

    Help

--- phase3/vmSupport.c
+++ phase3/vmSupport.c
@@ -92,8 +92,9 @@ void supLvlTlbExceptionHandler() {
 void uTLB_RefillHandler(){
     support_t *sPtr = (support_t *) SYSCALL (GETSUPPORTPTR, 0, 0, 0); /* Get the pointer to the Current Process’s Support Structure */
     int missingPN = (((*(state_PTR)BIOSDATAPAGE).s_entryHI & VPNMASK) >> VPNSHIFT) % PGTBLSIZE; /* Extract the missing page number from Entry HI */
-    debugVM(0xBADDBABE, missingPN, entry.entryHI, entry.entryLO);
     pte_entry_t entry = sPtr->sup_privatePgTbl[missingPN];  /* Get the Page Table entry for page number of the Current Process */
+    debugVM(0xBADDBABE, missingPN, entry.entryHI, entry.entryLO);
+    
     /* Write this Page Table entry into the TLB */
     setENTRYHI(entry.entryHI);  
     setENTRYLO(entry.entryLO);
