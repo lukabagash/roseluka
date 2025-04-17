a5aa760b3cc3f349dedc00d7e9a69f3ab27ffcde
Rosalie Lee <yeajin21346@gmail.com>
2025-04-16 20:26:46 -0400

    nvm

--- testers/psychoBreaker9000.c
+++ testers/psychoBreaker9000.c
@@ -2,29 +2,57 @@
 #include "h/tconst.h"
 #include "h/print.h"
 
+#define NUM_PAGES 28
+#define START_PAGE 2
+#define ITERATIONS 5
+
 void main() {
-	char input[40];
-	char reversed[40];
-	int status;
-	int i;
+    int i, j, idx, corrupt = 0;
+    char *pagePtr;
+    char inputBuf[20];
 
-	print(WRITETERMINAL, "Reverse String Test starts\n");
-	print(WRITETERMINAL, "Enter a string: ");
+    print(WRITETERMINAL, "=== psychSwapstorm.c initiating 5 times===\n");
 
-	status = SYSCALL(READTERMINAL, (int)&input[0], 0, 0);
-	input[status] = EOS;
+    for (i = 0; i < ITERATIONS; i++) {
+        print(WRITETERMINAL, "Swap iteration cycle");
+        print(WRITETERMINAL, "\n");
 
-	print(WRITETERMINAL, "\nOriginal string: ");
-	print(WRITETERMINAL, input);
+        for (j = START_PAGE; j < START_PAGE + NUM_PAGES; j++) {
+            pagePtr = (char *)(SEG2 + j * PAGESIZE);
+            pagePtr[(i * 17) % PAGESIZE] = j + i; /* random offset write */
+        }
 
-	for (i = 0; i < status - 1; i++) {
-		reversed[i] = input[status - 2 - i]; // -2 to skip newline and start from last char
-	}
-	reversed[status - 1] = EOS;
+        print(WRITETERMINAL, "Random writes to pages done.\n");
 
-	print(WRITETERMINAL, "\nReversed string: ");
-	print(WRITETERMINAL, reversed);
-	print(WRITETERMINAL, "\n\nReverse concluded\n");
+        /* Trigger user I/O interleaved with memory stress */
+        print(WRITETERMINAL, "Type a short string (will be echoed back): ");
+        SYSCALL(READTERMINAL, (int)&inputBuf[0], 0, 0);
+        inputBuf[18] = EOS; /* EOS somewhere */
+        print(WRITETERMINAL, "\nEchoing: ");
+        print(WRITETERMINAL, &inputBuf[0]);
+        print(WRITETERMINAL, "\n");
+    }
 
-	SYSCALL(TERMINATE, 0, 0, 0);
+    print(WRITETERMINAL, "Verifying data integrity...\n");
+
+    for (j = START_PAGE; j < START_PAGE + NUM_PAGES; j++) {
+        pagePtr = (char *)(SEG2 + j * PAGESIZE);
+        if (pagePtr[(ITERATIONS - 1) * 17 % PAGESIZE] != j + ITERATIONS - 1) {
+            print(WRITETERMINAL, "Data corrupted");
+            print(WRITETERMINAL, "\n");
+            corrupt = 1;
+        }
+    }
+
+    if (!corrupt)
+        print(WRITETERMINAL, "All page data intact after stress test!\n");
+    else
+        print(WRITETERMINAL, "Some data was corrupted! Pager fault?\n");
+
+    print(WRITETERMINAL, "Final phase: testing privilege enforcement...\n");
+    *((char *)(0x20000000)) = 42;
+
+    print(WRITETERMINAL, "ERROR: was able to write to ksegOS\n");
+
+    SYSCALL(TERMINATE, 0, 0, 0);
 }
