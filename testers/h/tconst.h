b15a42be7e83e0813b4538642295127ae1066c6d
lukabagash <bagash_l2@denison.edu>
2025-05-08 13:24:52 -0400

    revert

--- testers/h/tconst.h
+++ testers/h/tconst.h
@@ -24,8 +24,8 @@
 #define WRITEPRINTER	11
 #define WRITETERMINAL 	12
 #define READTERMINAL	13
-#define DISK_GET		15
-#define DISK_PUT		14
+#define DISK_GET		14
+#define DISK_PUT		15
 #define	FLASH_GET		16
 #define FLASH_PUT		17
 #define DELAY			18
